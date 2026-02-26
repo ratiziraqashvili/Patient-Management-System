b0e378f70d76c71c44bf91234d6ad0c04ea3ac8b
Rati <rziraqashvili@gmail.com>
2026-02-26 14:55:40 +0400

    created security config and password encoder

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -2,6 +2,7 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,9 +10,11 @@ import java.util.Optional;
 @Service
 public class AuthService {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
